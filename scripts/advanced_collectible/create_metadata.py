16399b844b96d2324ddcd5483e0ed4d94f781c4f
Ed-Marcavage <edmarcavage256@proton.me>
2022-10-03 17:32:08 -0400

    Adds set_tokenURI function

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -7,6 +7,12 @@ import json
 import requests
 from scripts.helpful_scripts import get_breed
 
+breed_to_image_uri = {
+    "PUG": "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
+    "SHIBA_INU": "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png",
+    "ST_BERNARD": "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
+}
+
 def main():
     print("Working on " + network.show_active())
     advanced_collectible = AdvancedCollectible[-1]
